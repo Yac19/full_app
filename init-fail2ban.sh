37ab494fcc734c2fb28b6d65b724926ba9344855
Yac19 <122814927+Yac19@users.noreply.github.com>
2023-09-12 16:57:23 +0200

    Update init-fail2ban.sh

--- init-fail2ban.sh
+++ init-fail2ban.sh
@@ -3,5 +3,8 @@ cd /var/run/fail2ban
 rm fail2ban.sock fail2ban.pid
 service fail2ban start
 
+# Lancer en arrière plan
+fail2ban-client start
+
 # Lancer le serveur Nginx en premier plan
 nginx -g "daemon off;"
